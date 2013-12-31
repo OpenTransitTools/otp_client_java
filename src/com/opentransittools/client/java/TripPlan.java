384f88fd7f0161472f4d67b35790758b26d5856c
Frank Purcell <fxpurcell@gmail.com>
2013-12-30 18:06:28 -0800

    add a simple .json test file for getting the system up & running, etc...

--- src/com/opentransittools/client/java/TripPlan.java
+++ src/com/opentransittools/client/java/TripPlan.java
@@ -3,17 +3,34 @@ package com.opentransittools.client.java;
 
 public class TripPlan
 {
+    private RequestParameters m_requestParameters;
+    private Plan m_plan;
+
+
     public static class RequestParameters
     {
+        private String m_time;
+
+        public String getTime() {return m_time;}
+        public void setTime(String time) {m_time = time; }
     }
+    public RequestParameters getRequestParameters() {return m_requestParameters;}
+    public void setRequestParameters(RequestParameters requestParameters) {m_requestParameters = requestParameters;}
 
     public static class Plan
     {
+        private From m_from;
+
         public static class From
         {
             private String m_name;
             public  String getName() { return m_name; }
             public  void   setName(String n) { m_name = n; }
         }
+        public From getFrom() {return m_from;          }
+        public void setFrom(From from) {m_from = from; }
+
     }
+    public Plan getPlan() {return m_plan;    }
+    public void setPlan(Plan plan) {m_plan = plan;    }
 }
