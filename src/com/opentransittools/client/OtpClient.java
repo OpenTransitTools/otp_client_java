fd2c6b57f7c4744ec9a0e34538c893203bfa8aba
Frank Purcell <fxpurcell@gmail.com>
2014-01-03 12:57:49 -0800

    inital add of the client logic

---
+++ src/com/opentransittools/client/OtpClient.java
@@ -0,0 +1,14 @@
+package com.opentransittools.client;
+
+import com.opentransittools.client.TripPlan;
+
+/**
+ * OtpClient does three things:
+ * 1)  provides a set of http get parameters for OTP (with default values)
+ * 2a) calls OTP via http
+ * 2b) marshal's the OTP response into the com.opentransittools.client.TripPlan client POJO.
+ */
+public class OtpClient
+{
+    
+}
