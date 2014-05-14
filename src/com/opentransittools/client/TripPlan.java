da25671556e7313b14991d1827898286e34dbb3d
Frank Purcell <fxpurcell@gmail.com>
2014-05-14 12:25:02 -0700

    don't need list ref

--- src/com/opentransittools/client/TripPlan.java
+++ src/com/opentransittools/client/TripPlan.java
@@ -1,7 +1,5 @@
 package com.opentransittools.client;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
