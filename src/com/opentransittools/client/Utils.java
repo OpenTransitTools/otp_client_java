1b0de81f5ac1434724739e87b0604d5e766bbbd8
Frank Purcell <fxpurcell@gmail.com>
2014-06-13 13:51:02 -0700

    add utils class...

---
+++ src/com/opentransittools/client/Utils.java
@@ -0,0 +1,26 @@
+package com.opentransittools.client;
+
+public class Utils
+{
+
+    public static class Modes
+    {
+        public static Boolean isWalk(String mode)
+        {
+            Boolean ret_val = false;
+            return ret_val;
+        }
+
+        public static Boolean isBike(String mode)
+        {
+            Boolean ret_val = false;
+            return ret_val;
+        }
+
+        public static Boolean isTransit(String mode)
+        {
+            Boolean ret_val = false;
+            return ret_val;
+        }
+    }
+}
