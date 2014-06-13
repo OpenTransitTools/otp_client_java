8b96807d680fa90a2823d1af1019d625cbac5f5c
Frank Purcell <fxpurcell@gmail.com>
2014-06-13 14:28:52 -0700

    mode utils ... isTransit, etc...

--- src/com/opentransittools/client/Utils.java
+++ src/com/opentransittools/client/Utils.java
@@ -1,26 +1,48 @@
 package com.opentransittools.client;
 
+import java.util.regex.Pattern;
+
 public class Utils
 {
 
     public static class Modes
     {
+        public static String BUS     = "BUS";
+        public static String TRAIN   = "TRAIN";
+        public static String RAIL    = "RAIL";
+        public static String TRAM    = "TRAM";
+        public static String GONDOLA = "GONDOLA";
+
+        public static String CAR     = "CAR";
+
+        public static String WALK    = "WALK";
+        public static String BICYCLE = "BICYCLE";
+
+        public static String TRANSIT = BUS + "|" + TRAIN + "|" + RAIL + "|" + TRAM + "|" + GONDOLA;
+        public static Pattern TRANSIT_PATTERN = Pattern.compile(TRANSIT);
+
         public static Boolean isWalk(String mode)
         {
             Boolean ret_val = false;
+            if(mode != null && mode.equals(WALK))
+                ret_val = true;
             return ret_val;
         }
 
         public static Boolean isBike(String mode)
         {
             Boolean ret_val = false;
+            if(mode != null && mode.equals(BICYCLE))
+                ret_val = true;
             return ret_val;
         }
 
         public static Boolean isTransit(String mode)
         {
             Boolean ret_val = false;
+            if(mode != null && mode.matches(TRANSIT))
+                ret_val = true;
             return ret_val;
         }
-    }
+   }
 }
